18d03e7708124c79896ccccd21c9d2ad56d4aef9
Kyle Owsen <kyle.owsen@gmail.com>
2015-12-26 15:17:06 -0800

    Wrap main.js in anonymous function

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,16 @@
 'use strict';
 
-var stateManager = new StateManager();
-var audioManager = new AudioManager(stateManager.registerCallback, function() {
-	return stateManager.getOption("enableTownTune");
-});
-var notificationManager = new NotificationManager(stateManager.registerCallback, function() {
-	return stateManager.getOption("enableNotifications");
-});
-var badgeManager = new BadgeManager(stateManager.registerCallback);
-
-stateManager.activate();
+(function() {
+	
+	var stateManager = new StateManager();
+	var audioManager = new AudioManager(stateManager.registerCallback, function() {
+		return stateManager.getOption("enableTownTune");
+	});
+	var notificationManager = new NotificationManager(stateManager.registerCallback, function() {
+		return stateManager.getOption("enableNotifications");
+	});
+	var badgeManager = new BadgeManager(stateManager.registerCallback);
+	
+	stateManager.activate();
+	
+})();
