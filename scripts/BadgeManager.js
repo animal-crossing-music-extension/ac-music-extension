2c2bdef764978558ceef21eb9742285db0c8fbaf
mat <32943178+matlsn@users.noreply.github.com>
2019-03-29 20:22:58 -0400

    Fix badge hour formatting and refactors

--- scripts/BadgeManager.js
+++ scripts/BadgeManager.js
@@ -3,44 +3,21 @@
 'use strict';
 
 function BadgeManager(addEventListener, isEnabled) {
-
+	
 	addEventListener("hourMusic", function(hour) {
-		if(isEnabled()) {
-			chrome.browserAction.setBadgeText({ text: formatHour(hour) });
-		}
-		changeIcon(true, false);
+		isEnabled() && chrome.browserAction.setBadgeText({ text: `${formatHour(hour)}m` });
+		chrome.browserAction.setIcon({ path: 'img/icon_38_leaf_playing.png' });
 	});
 
 	addEventListener("kkStart", function() {
-		if(isEnabled()) {
-			chrome.browserAction.setBadgeText({ text: "KK" });
-		}
-		changeIcon(true, true);
+		isEnabled() && chrome.browserAction.setBadgeText({ text: "KK" });
+		chrome.browserAction.setIcon({ path: 'img/icon_38_kk_playing.png' });
 	});
 
 	addEventListener("pause", function() {
 		chrome.browserAction.setBadgeText({ text: "" });
-		changeIcon(false, false);
+		chrome.browserAction.setIcon({ path: 'img/icon_38_leaf_paused.png' });
 	});
 	
-	function changeIcon(isPlaying, isKK) {
-		if(isPlaying) {
-			if(isKK) {
-				chrome.browserAction.setIcon({
-					path : "img/icon_38_kk_playing.png"
-				});
-			} else {
-				chrome.browserAction.setIcon({
-					path : "img/icon_38_leaf_playing.png"
-				});
-			}		
-		}
-		else {
-		chrome.browserAction.setIcon({
-			path : "img/icon_38_leaf_paused.png"
-		});
-		}
-	}
-
 	chrome.browserAction.setBadgeBackgroundColor({ color: [57, 230, 0, 255] });
 }
