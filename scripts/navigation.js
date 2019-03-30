90c81966e85ac3de92b59bfe7755d100fb9600f0
mat <32943178+matlsn@users.noreply.github.com>
2019-03-29 20:30:26 -0400

    jQuery refactors and double-init fixes

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,48 +1,46 @@
 $(function() {
+  var body = $('body');
   $('.menu a').click(function(ev) {
     ev.preventDefault();
     var selected = 'selected';
 
-    $('.mainview > *').removeClass(selected);
-    $('.menu li').removeClass(selected);
+    $('.mainview > *, .menu li').removeClass(selected);
     setTimeout(function() {
-      $('.mainview > *:not(.selected)').css('display', 'none');
+      $('.mainview > *').not('.selected').css('display', 'none');
     }, 100);
 
     $(ev.currentTarget).parent().addClass(selected);
-    var currentView = $($(ev.currentTarget).attr('href'));
-    currentView.css('display', 'block');
+    var currentView = $($(ev.currentTarget).attr('href')).css('display', 'block');
     setTimeout(function() {
       currentView.addClass(selected);
     }, 0);
 
     setTimeout(function() {
-      $('body')[0].scrollTop = 0;
+      body[0].scrollTop = 0;
     }, 200);
   });
 
   $('#launch_modal').click(function(ev) {
     ev.preventDefault();
     var modal = $('.overlay').clone();
-    $(modal).removeAttr('style');
-    $(modal).find('button, .close-button').click(function() {
-      $(modal).addClass('transparent');
+    var modalPage = modal.find('.page');
+    modal.removeAttr('style').find('button, .close-button').click(function() {
+      modal.addClass('transparent');
       setTimeout(function() {
-        $(modal).remove();
+        modal.remove();
       }, 1000);
     });
 
-    $(modal).click(function() {
-      $(modal).find('.page').addClass('pulse');
-      $(modal).find('.page').on('webkitAnimationEnd', function() {
+    modal.click(function() {
+      modalPage.addClass('pulse').on('webkitAnimationEnd', function() {
         $(this).removeClass('pulse');
       });
     });
-    $(modal).find('.page').click(function(ev) {
+    modalPage.click(function(ev) {
       ev.stopPropagation();
     });
-    $('body').append(modal);
+    body.append(modal);
   });
-
-  $('.mainview > *:not(.selected)').css('display', 'none');
+  
+  $('.mainview > *').not('.selected').css('display', 'none');
 });
