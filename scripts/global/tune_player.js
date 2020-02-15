26ac53bd6724737bdb6b01013aafa89bcc9232e6
Julian-MJK <julian.kragset@gmail.com>
2020-02-15 06:01:14 +0100

    Add: Town-tune volume, '?' pitch & higher pitches

--- scripts/global/tune_player.js
+++ scripts/global/tune_player.js
@@ -1,78 +1,102 @@
 (function() {
-  var availablePitches = ['zZz', '-', 'G1', 'A1', 'B1', 'C2', 'D2', 'E2', 'F2', 'G2', 'A2', 'B2', 'C3', 'D3', 'E3'];
+  var availablePitches = ['zZz', '-', 'F1', 'G1', 'A1', 'B1', 'C2', 'D2', 'E2', 'F2', 'G2', 'A2', 'B2', 'C3', 'D3', 'E3', '?'];
+  var frequencies      = [null,  null, 350,  392,  440,  494,  523,  587,  659,  698,  784,  880,  988, 1046, 1174, 1318, "random"];
+  // ^ values in HZ
+  
+  var _defaultTownTuneVolume = 0.75;
+  
+  
+  /**
+   * @function createBooper
+   * @desc  Creates & returns method responsible for playing town-tune notes in the town-tune editor
+   * @param {*} audioContext 
+   * @returns {method} playNote
+   */
+  var createBooper = function(audioContext) { 
+    
+    var attack = 0.05;  //in seconds
+    var decay = 0.1;    //in seconds
+    var release = 0.15; //in seconds
+    var gainLevel = 3;
+    var sustainLevel = 2;
+    var cutoffModifier = 8;
+    var Q = 0; 
+    
+    
+    var pitchToFreq = function(pitch) {
+      if (typeof pitch == 'number') return pitch;
+      
+      index = availablePitches.indexOf(pitch);
+      if (index == -1) return null; // Pitch does not exist in register
+      
+      var freq = frequencies[index];
+      
+      // Generating random frequency
+      if(freq == "random") freq = frequencies[ 2 + Math.floor( Math.random() * (frequencies.length - 3) ) ];  
+      
+      if (!freq) return null; // Pitch not assigned to a frequency
 
+      return freq;
+    };
 
-var createBooper = function(audioContext) {
-  //values in HZ
-  var frequencies = [null, null, 392, 440, 494, 523, 587, 659, 698, 784, 880, 988, 1046, 1174, 1318];
+    var playNote = function(pitch, time, sustainDuration) {
+      if (time === undefined) time = audioContext.currentTime;
+      if (sustainDuration === undefined) sustainDuration = 0;
 
-  var attack = 0.05; //in seconds
-  var decay = 0.1; //in seconds
-  var release = 0.15; //in seconds
-  var gainLevel = 3;
-  var sustainLevel = 2;
-  var cutoffModifier = 8;
-  var Q = 0;
+      var freq = pitchToFreq(pitch);
+      if (!freq) return;
 
-  var pitchToFreq = function(pitch) {
-    if (typeof pitch == 'number') return pitch;
+      var oscillator, filter, gain;
 
-    index = availablePitches.indexOf(pitch);
-    if (index == -1) return null;
+      oscillator = audioContext.createOscillator();
+      oscillator.type = 'square';
+      oscillator.frequency.value = freq;
 
-    var freq = frequencies[index]
-    if (!freq) return null;
+      filter = audioContext.createBiquadFilter();
+      filter.type = 'lowpass';
+      //filter tracks the note being played
+      filter.frequency.value = Math.sqrt(freq) * cutoffModifier;
+      filter.Q.value = Q;
 
-    return freq;
+      gain = audioContext.createGain();
+      gain.gain.value = 0;
+
+      oscillator.connect(filter);
+      filter.connect(gain);
+      gain.connect(audioContext.destination);
+
+      oscillator.start(time);
+      oscillator.stop(time + attack + decay + sustainDuration + release);
+
+      // Fetching stored or default town tune volume
+      chrome.storage.sync.get({townTuneVolume: _defaultTownTuneVolume}, function(items){
+        let volume = items.townTuneVolume;
+        
+        gain.gain.setValueAtTime         (0,                  time);
+        gain.gain.linearRampToValueAtTime(gainLevel    * volume, time + attack);
+        gain.gain.linearRampToValueAtTime(sustainLevel * volume, time + attack + decay);
+        gain.gain.setValueAtTime         (sustainLevel * volume, time + attack + decay + sustainDuration);
+        gain.gain.linearRampToValueAtTime(0,                  time + attack + decay + sustainDuration + release);
+        
+      });
+    };
+
+    return {
+      playNote: playNote
+    }
   };
 
-  var playNote = function(pitch, time, sustainDuration) {
-    if (time === undefined) time = audioContext.currentTime;
-    if (sustainDuration === undefined) sustainDuration = 0;
-
-    var freq = pitchToFreq(pitch);
-    if (!freq) return;
-
-    var oscillator, filter, gain;
-
-    oscillator = audioContext.createOscillator();
-    oscillator.type = 'square';
-    oscillator.frequency.value = freq;
-
-    filter = audioContext.createBiquadFilter();
-    filter.type = 'lowpass';
-    //filter tracks the note being played
-    filter.frequency.value = Math.sqrt(freq) * cutoffModifier;
-    filter.Q.value = Q;
-
-    gain = audioContext.createGain();
-    gain.gain.value = 0;
-
-    oscillator.connect(filter);
-    filter.connect(gain);
-    gain.connect(audioContext.destination);
-
-    oscillator.start(time);
-    oscillator.stop(time + attack + decay + sustainDuration + release);
-
-    gain.gain.setValueAtTime(0, time);
-    gain.gain.linearRampToValueAtTime(gainLevel, time + attack);
-    gain.gain.linearRampToValueAtTime(sustainLevel, time + attack + decay);
-    gain.gain.setValueAtTime(sustainLevel, time + attack + decay + sustainDuration);
-    gain.gain.linearRampToValueAtTime(0, time + attack + decay + sustainDuration + release);
-  };
-
-  return {
-    playNote: playNote
-  }
-};
-
-
+  
+/**
+ * @function createSampler
+ * @desc  Creates & (?) returns method responsible for playing notes used to play the town tune at the hour
+ * @param {*} audioContext 
+ * @returns {method} playNote
+ */
 var createSampler = function(audioContext) {
   var bellBuffer;
   var startPoints = [null, null];
   var chimeLength = 3.8;
-  var volume = 0.2;
 
   var pitchToStartPoint = function(pitch) {
     index = availablePitches.indexOf(pitch);
@@ -99,22 +123,33 @@ var createSampler = function(audioContext) {
   };
 
   var initStartPoints = function() {
-    for (var i = 0; i < availablePitches.length - 2; i++) {
+    for (var i = 0; i < availablePitches.length; i++) {
       startPoints.push(i * 4);
     }
   };
 
-  var playNote = function(pitch, time, sustain) {
+  var playNote = function(pitch, time, sustain) {  //Sustain parameter isn't used here (unlike at createBooper.playNote)
     if (!bellBuffer) return;
     var source = audioContext.createBufferSource();
     source.buffer = bellBuffer;
-
-    gain = audioContext.createGain();
-    gain.gain.value = volume;
-
-    source.connect(gain);
-    gain.connect(audioContext.destination);
-    source.start(time, pitchToStartPoint(pitch), chimeLength);
+    
+    // If pitch is '?', randomizing pitch
+    if(pitch == '?') pitch = availablePitches[ 2 + Math.floor( Math.random() * (availablePitches.length - 3) ) ]
+    
+    // Fetching volume
+    chrome.storage.sync.get({townTuneVolume: _defaultTownTuneVolume}, function(items){
+      let volume = items.townTuneVolume * 0.5;
+      // The notes sound broken & not accurate to the editor's volume when played at higher volumes, vol is therefore reduced with this multiplier.
+      
+      // Configuring gain
+      gain = audioContext.createGain();
+      gain.gain.value = volume;
+      
+      // Playing audio
+      source.connect(gain);
+      gain.connect(audioContext.destination);
+      source.start(time, pitchToStartPoint(pitch), chimeLength); 
+    });
   };
 
   initStartPoints();
@@ -126,6 +161,14 @@ var createSampler = function(audioContext) {
 };
 
 
+
+/**
+ * @function createTunePlayer
+ * @desc  Creates & returns object responsible for handling the playing of town tunes at the hour.
+ * @param {*} audioContext 
+ * @param {*} bpm 
+ * @returns {object} tunePlayer 
+ */
 var createTunePlayer = function(audioContext, bpm) {
   var defaultBpm = 240.0; // BPM
   var stepDuration;
@@ -170,7 +213,10 @@ var createTunePlayer = function(audioContext, bpm) {
       if(pitch == rest || pitch == sustain) continue;
 
       sustainDuration = getSustainMultiplier(i, tune) * stepDuration;
-      instrument.playNote(pitch, audioContext.currentTime + time, sustainDuration);
+      // Plays a note using createSampler.playNote() method
+      instrument.playNote(pitch, audioContext.currentTime + time, sustainDuration); 
+      // The sustain parameter is however, not used. 
+      // Look at createBooper.playNote() method for a way to implement sustain in createSampler.playNote().
     }
 
     //jQuery stlye chain callbacks
