4c33f15b1d6a31c920ec1bb5dd158f99eda7cefb
Jasmine G <paladique@users.noreply.github.com>
2020-03-16 20:36:41 -0400

    change contributors table to grid

--- scripts/options/contributors.js
+++ scripts/options/contributors.js
@@ -37,80 +37,72 @@ async function updateContributors() {
         return;
     }
 
-    let contributorsTable = document.getElementById('contributors-table');
-    let totalRows = Math.ceil(contributors.length / tableColCount);
-    let rows = [];
+    let contributorsContainer = document.getElementById('contributors-container');
 
-    for (let i = 0; i < totalRows; i++) {
-        rows.push(document.createElement('tr'));
-    }
 
-    rows.forEach((row, rowI) => {
-        // Done in this funky way rather than just a forEach to easily create empty table columns to complete the table with an odd amount of contributors.
-        for (let cI = rowI * tableColCount; cI < tableColCount + rowI * tableColCount; cI++) {
-            let contributor = contributors[cI];
-            let td = document.createElement('td');
-            td.className = 'contributor';
+    contributors.forEach((contributor) => {
+        let td = document.createElement('div');
+        td.className = 'contributor';
 
-            if (contributor) {
-                let avatar = document.createElement('img');
-                avatar.src = contributor.avatar_url;
+        if (contributor) {
+            let avatar = document.createElement('img');
+            avatar.src = contributor.avatar_url;
 
-                let name = document.createElement('p');
-                name.textContent = contributor.name;
+            let name = document.createElement('p');
+            name.textContent = contributor.name;
 
-                let contributions;
-                if (contributor.contributions > 0) {
-                    contributions = document.createElement('span');
-                    contributions.textContent = `${contributor.contributions} ${(contributor.contributions == 1) ? 'Contribution' : 'Contributions'}`;
-                }
-
-                let githubLink;
-                if (contributor.url) {
-                    githubLink = document.createElement('a');
-                    githubLink.target = "_blank";
-                    githubLink.href = contributor.url;
-                    githubLink.className = 'contributorLink';
-
-                    let icon = document.createElement('img');
-                    icon.src = 'img/social/github.png';
-                    icon.className = 'githubIcon';
-                    githubLink.appendChild(icon);
-
-                    githubLink.append(`@${contributor.login}`);
-                }
-
-                let discordName;
-                if (contributor.discord) {
-                    discordName = document.createElement('span');
-                    discordName.className = 'contributorLink';
-
-                    let icon = document.createElement('img');
-                    icon.src = 'img/social/discord.png';
-                    icon.className = 'discordIcon';
-                    discordName.appendChild(icon);
-
-                    discordName.append(`@${contributor.discord}`);
-                }
-
-                td.appendChild(avatar);
-                td.appendChild(name);
-                if (contributions) {
-                    td.appendChild(document.createElement('br'));
-                    td.appendChild(contributions);
-                }
-                if (githubLink) {
-                    td.appendChild(document.createElement('br'));
-                    td.appendChild(githubLink);
-                }
-                if (discordName) {
-                    td.appendChild(document.createElement('br'));
-                    td.appendChild(discordName);
-                }
+            let contributions;
+            if (contributor.contributions > 0) {
+                contributions = document.createElement('span');
+                contributions.textContent = `${contributor.contributions} ${(contributor.contributions == 1) ? 'Contribution' : 'Contributions'}`;
             }
 
-            row.appendChild(td);
+            let githubLink;
+            if (contributor.url) {
+                githubLink = document.createElement('a');
+                githubLink.target = "_blank";
+                githubLink.href = contributor.url;
+                githubLink.className = 'contributorLink';
+
+                let icon = document.createElement('img');
+                icon.src = 'img/social/github.png';
+                icon.className = 'githubIcon';
+                githubLink.appendChild(icon);
+
+                githubLink.append(`@${contributor.login}`);
+            }
+
+            let discordName;
+            if (contributor.discord) {
+                discordName = document.createElement('span');
+                discordName.className = 'contributorLink';
+
+                let icon = document.createElement('img');
+                icon.src = 'img/social/discord.png';
+                icon.className = 'discordIcon';
+                discordName.appendChild(icon);
+
+                discordName.append(`@${contributor.discord}`);
+            }
+
+            td.appendChild(avatar);
+            td.appendChild(name);
+            if (contributions) {
+                td.appendChild(document.createElement('br'));
+                td.appendChild(contributions);
+            }
+            if (githubLink) {
+                td.appendChild(document.createElement('br'));
+                td.appendChild(githubLink);
+            }
+            if (discordName) {
+                td.appendChild(document.createElement('br'));
+                td.appendChild(discordName);
+            }
         }
-        contributorsTable.appendChild(row);
+
+        contributorsContainer.appendChild(td);
+
+
     });
 }
